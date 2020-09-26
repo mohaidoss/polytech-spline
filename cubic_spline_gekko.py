9729049d38d7bd42b75654eb99dcd8aea86a8f39
Mehdi Moussaif <mehdi.moussaif.etu@univ-lille.fr>
2020-09-26 15:56:27 +0200

    Cubic Spline avec GEKKO

--- cubic_spline_gekko.py
+++ cubic_spline_gekko.py
@@ -8,27 +8,20 @@ b = 5
 n = 5
 
 points = np.empty((n + 1,2)) #Ensemble de points
-xn = np.linspace(a,b,n+1)  #subdivision des abscisses de a à b
-yn = np.array([0.1,0.2,0.3,0.5,1.0,0.9])
-points[:,0] = xn
+data = np.array([[0,0.1],[1,0.4],[2,0.2],[3,-0.2],[4,0.1],[5,0.5],[6,0.6]])  #Ensemble de points
+xn = data[:,0]  #abscisses des points
+yn = data[:,1]  #ordonnées des points 
 
 m = GEKKO()
-m.x = m.Param(value = np.linspace(-1,6))
+m.x = m.Param(value = np.linspace(-1,7))
 m.y = m.Var()
 m.options.IMODE=2
 m.cspline(m.x,m.y,xn,yn)
 m.solve(disp=False)
 #help(m.cspline)
 
-p = GEKKO()
-p.x = p.Var(value=1,lb=0,ub=5)
-p.y = p.Var()
-p.cspline(p.x,p.y,xn,yn)
-p.Obj(-p.y)
-p.solve(disp=False)
 
 plt.plot(xn,yn,'bo',label='data')
 plt.plot(m.x.value,m.y.value,'r--',label='cubic spline')
-plt.plot(p.x.value,p.y.value,'ko',label='maximum')
 plt.legend(loc='best')
 plt.show()
